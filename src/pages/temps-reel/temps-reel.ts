a594bf5412a242846dc6b6ab97477f9be33f7748
AUTOMOTIVE\BOUTOUIL <Karim.Boutouil@malix.univ-paris1.fr>
2017-12-08 03:29:24 +0100

    fix bug

--- src/pages/temps-reel/temps-reel.ts
+++ src/pages/temps-reel/temps-reel.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-<<<<<<< HEAD
 import { LoginPage } from '../login/login';
-=======
 import { Geolocation } from '@ionic-native/geolocation';
+import { GoogleMap } from "@ionic-native/google-maps";
 
 
->>>>>>> f6ca7c0ada5dbcd50334441a2da02be9dd1a9bda
 @Component({
     selector: 'temps-reel-page',
     templateUrl: 'temps-reel.html'
@@ -15,31 +13,28 @@ export class TempsReelPage {
     map: GoogleMap;
     constructor(public navCtrl: NavController, private geolocation: Geolocation) {
         this.geolocation.getCurrentPosition().then((resp) => {
-          
+
             this.initMap();
 
         }).catch((error) => {
             console.log('Error getting location', error);
         });
     }
-    
- 
+
+
 
     initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 13,
-        center: { lat: 34.04924594193164, lng: -118.24104309082031 }
-    });
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 13,
+            center: { lat: 34.04924594193164, lng: -118.24104309082031 }
+        });
+        var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);
+    }
+    goHome() {
+        this.navCtrl.setRoot(LoginPage);
+    }
 
-<<<<<<< HEAD
-  }
-  goHome(){
-  	this.navCtrl.setRoot(LoginPage);
-  }
-=======
-    var trafficLayer = new google.maps.TrafficLayer();
-    trafficLayer.setMap(map);
 }
 
->>>>>>> f6ca7c0ada5dbcd50334441a2da02be9dd1a9bda
 }
